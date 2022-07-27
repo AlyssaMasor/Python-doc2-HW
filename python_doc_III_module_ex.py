44fb2688a9dadd904c01771e2a0ee6b87b6e43c7
Alyssa <pufina13@gmail.com>
2022-07-26 20:30:03 -0400

    second update with III

---
+++ python_doc_III_module_ex.py
@@ -0,0 +1,11 @@
+def square_foot(legnth, width):
+    area = legnth * width 
+    print(area)
+
+
+
+def circle_circumference(diameter):
+    circumference = diameter * 3.14
+    print(circumference)
+
+
